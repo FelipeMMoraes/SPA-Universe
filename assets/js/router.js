58b49a71732e854da0b2d013709aa3df711b627e
FelipeMMoraes <felipe.moraes8@hotmail.com>
2022-09-21 01:23:44 -0300

    fixing 404 error

--- assets/js/router.js
+++ assets/js/router.js
@@ -24,7 +24,7 @@ export class Router {
 
   handle() {
     const { pathname } = window.location
-    const route = this.routes[pathname]
+    const route = this.routes[pathname] || this.routes[404]
 
     fetch(route)
     .then(data => data.text())
