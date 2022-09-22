12a972d9b79796cb3a7b596d2cf9c0ec9797f446
FelipeMMoraes <felipe.moraes8@hotmail.com>
2022-09-22 20:22:23 -0300

    nav menu mobile

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,31 @@
-import { Router } from './router.js'
+import { Router } from "./router.js";
 
-const router = new Router()
-router.add('/', "/pages/home.html")
-router.add('/universe', "/pages/universe.html")
-router.add('/exploration', "/pages/exploration.html")
-router.add(404, "/pages/404.html")
+const router = new Router();
+router.add("/", "/pages/home.html");
+router.add("/universe", "/pages/universe.html");
+router.add("/exploration", "/pages/exploration.html");
+router.add(404, "/pages/404.html");
 
-router.handle()
+router.handle();
 
-window.onpopstate = () => router.handle()
-window.route = () => router.route()
+window.onpopstate = () => router.handle();
+window.route = () => router.route();
+
+const hamburguer = document.querySelector(".hamburguer");
+const navmenu = document.querySelector(".nav-menu");
+
+hamburguer.addEventListener("click", () => {
+  hamburguer.classList.toggle("active");
+  navmenu.classList.toggle("active");
+});
+
+const navItems = document.querySelectorAll(".nav-item");
+
+navItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (hamburguer.classList.contains("active")) {
+      hamburguer.classList.remove("active");
+      navmenu.classList.remove("active");
+    }
+  });
+});
--- assets/js/router.js
+++ assets/js/router.js
@@ -1,35 +1,35 @@
 export class Router {
-  routes = {}
+  routes = {};
 
   add(routeName, page) {
-    this.routes[routeName] = page
+    this.routes[routeName] = page;
   }
 
   route(event) {
-    event = event || window.event
-    event.preventDefault()
+    event = event || window.event;
+    event.preventDefault();
 
-    const parentAnchor = event.target.closest("a") 
-    
+    const parentAnchor = event.target.closest("a");
+
     if (parentAnchor.href) {
-      window.history.pushState({}, "", parentAnchor.href)
-      this.handle()
-      return
+      window.history.pushState({}, "", parentAnchor.href);
+      this.handle();
+      return;
     }
 
-    window.history.pushState({}, "", event.target.href)
+    window.history.pushState({}, "", event.target.href);
 
-    this.handle()
+    this.handle();
   }
 
   handle() {
-    const { pathname } = window.location
-    const route = this.routes[pathname] || this.routes[404]
+    const { pathname } = window.location;
+    const route = this.routes[pathname] || this.routes[404];
 
     fetch(route)
-    .then(data => data.text())
-    .then(html => {
-      document.querySelector('#app').innerHTML = html
-    })
+      .then((data) => data.text())
+      .then((html) => {
+        document.querySelector("#app").innerHTML = html;
+      });
   }
 }
